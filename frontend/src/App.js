3aaff9c327d3cc9fe445d4ad114dc1c4cdaa6443
tam986 <39400292+tam986@users.noreply.github.com>
2025-01-13 09:17:44 +0700

    cap nhat 13-1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Footer from './Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sptrongloai from './Sptrongloai';
+import Gioithieu from './Gioithieu';
+import Chitiet from './Chitiet';
+import Notfound from './Notfound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <BrowserRouter basename='/'>
+        <header><Header /></header>
+        <main className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sp/:id" element={<Chitiet />} />
+            <Route path="/loai" element={<Sptrongloai />} />
+          
+            <Route path="/loai/:id_loai" element={<Sptrongloai />} />
+            <Route path="/gioithieu" element={<Gioithieu />} />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </main>
+        
+        <footer className='container-fluid bg-body-tertiary p-0'><Footer /></footer>
+      </BrowserRouter>
     </div>
   );
 }
